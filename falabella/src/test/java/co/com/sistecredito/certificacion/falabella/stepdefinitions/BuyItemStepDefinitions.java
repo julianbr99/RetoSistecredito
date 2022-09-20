6640a55a95843298f01850e491cf7051f0688a8d
Julian Becerra Restrepo <julianbecerrarepo@gmail.com>
2022-09-20 14:24:17 -0500

    readme sistecredito

--- falabella/src/test/java/co/com/sistecredito/certificacion/falabella/stepdefinitions/BuyItemStepDefinitions.java
+++ falabella/src/test/java/co/com/sistecredito/certificacion/falabella/stepdefinitions/BuyItemStepDefinitions.java
@@ -8,6 +8,7 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import net.serenitybdd.screenplay.actions.Open;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 
@@ -26,6 +27,7 @@ public class BuyItemStepDefinitions {
     public void preparation() {
         setTheStage(new OnlineCast());
         theActorCalled(JULIAN);
+        WebDriverManager.chromedriver().setup();
     }
 
 
